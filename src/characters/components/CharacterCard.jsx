9cc150b8d01d7da05e25191d5f05d0a441b9693c
idriverag4 <idrivera4@misena.edu.co>
2022-07-19 21:57:28 -0500

    Se crea el modulo de la lista de los personajes. Se consume la api con ayuda de apollo

--- src/characters/components/CharacterCard.jsx
+++ src/characters/components/CharacterCard.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-export const CharacterCard = () => {
-  return (
-    <div>CharacterCard</div>
-  )
+export const CharacterCard = ( {id, name, image,  gender, status} ) => {
+    return (
+        <div className="col">
+            <div className="card animate__animated animate__fadeIn">
+                <div className="row no-gutters">
+
+                    <div className="col-6 ">
+                        <img src={image} className="card-img" alt={name} width="200" height="200" />
+                    </div>
+
+                    <div className="col-6">
+                        <div className="card-body">
+
+                            <h5 className="card-title">{name}</h5>
+
+                            <p className="card-text"> Gender: {gender}</p>
+                            <p className="card-text"> Status: {status}</p>
+
+                            <Link to={`/character/${id}`} >Más...</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/characters/components/CharacterList.jsx
+++ src/characters/components/CharacterList.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { useQuery } from '@apollo/client';
+import { GET_RESULT } from '../../apollo/querys/charactersQuery';
+// import { GET_RESULT_ID } from '../../apollo/querys/charactersQuery';
+import { CharacterCard } from "./CharacterCard";
 
 export const CharacterList = () => {
-  return (
-    <div>CharacterList</div>
-  )
+    
+    const { loading, error, data } = useQuery(GET_RESULT);
+
+    if (loading) return <p>Loading....</p>;
+    if (error) return <p>{error}</p>;
+    
+    return (
+
+        <div className="row rows-cols-1 row-cols-md-3 g-3 ">
+            {
+                data.characters.results.map( ( {id, name, image, gender, status} ) => (
+
+                    <CharacterCard
+                        key={id}
+                        name={name}
+                        image={image}
+                        gender={gender}
+                        status={status}
+                    />
+                ))
+            } 
+
+        </div>
+
+    )
 }
+
