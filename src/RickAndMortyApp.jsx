eea15bbc5586ecc7892af6994f14e355f1b8a547
idriverag4 <idrivera4@misena.edu.co>
2022-07-22 22:44:21 -0500

    build

--- src/RickAndMortyApp.jsx
+++ src/RickAndMortyApp.jsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 export const RickAndMortyApp = () => {
     return (
         <Provider store={store}>
-            <AppRouter />
+            <AppRouter /> 
         </Provider>
     )
 }
